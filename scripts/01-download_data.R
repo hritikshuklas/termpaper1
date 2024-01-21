e533b519c546263d554b456302fc2b7883a8eac6
Hritik Shukla <hritikshuklas@gmail.com>
2024-01-21 17:47:10 -0500

    updated download_data, added raw csv

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,16 +11,28 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Code snippet taken from:
+# https://open.toronto.ca/dataset/toronto-beaches-observations/
 
+# get package
+package <- show_package("toronto-beaches-observations")
+package
+
+# get all resources for this package
+resources <- list_package_resources("toronto-beaches-observations")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(data, "inputs/data/raw_data.csv") 
 
          
