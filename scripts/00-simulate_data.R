33826ac99f9eb1cc58c38362e456be590b5f9134
hritikshuklas <hritikshuklas@gmail.com>
2024-01-22 14:12:33 -0500

    Completed simulated data, finished data  tests

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -23,30 +23,30 @@ simulated_data <-
       max = 15000,
       min = 9000
     ) |> floor(),
+    thefts = rnorm(
+      n = num_neighbourhoods,
+      mean = 300,
+      sd = 20
+    ) |> floor(),
+    assaults = rnorm(
+      n = num_neighbourhoods,
+      mean = 210,
+      sd = 25
+    ) |> floor(),
+    homicides = rnorm(
+      n = num_neighbourhoods,
+      mean = 50,
+      sd = 15
+    ) |> floor(),
     theft_rate = rnorm(
       n = num_neighbourhoods,
       mean = 30,
       sd = 9
     ),
-    autotheft_rate = rnorm(
-      n = num_neighbourhoods,
-      mean = 50,
-      sd = 12
-    ),
-    robbery_rate = rnorm(
-      n = num_neighbourhoods,
-      mean = 15,
-      sd = 3
-    ),
     assault_rate = rnorm(
       n = num_neighbourhoods,
       mean = 40,
       sd = 8
-    ),
-    shooting_rate = rnorm(
-      n = num_neighbourhoods,
-      mean = 20,
-      sd = 4
     ),
     homicide_rate = rnorm(
       n = num_neighbourhoods,
@@ -65,37 +65,41 @@ assert_col_count <- function(df, n){
   return(ncol(df) == n)
 }
 
-# Check if no entry contains NULL string
-# Returns TRUE  if no entries are NULL string
-#         FALSE if at least one entry is NULL string
-ans <- is.null(simulated_data)
-assert_no_null_entries <- any(ans)
-
-# Check if no entry contains NA
-# Returns TRUE  if no entries are NA
-#         FALSE if at least one entry is NA
-ans <- is.na(simulated_data)
-assert_no_na_entries <- any(ans)
-
-# Check if no entry contains NaN
-# Returns TRUE  if no entries are NaN
-#         FALSE if at least one entry is NaN
-nan(simulated_data)
-assert_no_nan_entries <- any(ans)
-
-# Check if population is a positive integer
-# Returns TRUE  if all entries are positive integers
-#         FALSE if at least one entry is a non-positive integer
-test_positive_integers <- function(col){
-  ans <- TRUE
-  for (x in col){
-      ans <- (ans && is.numeric(x) && x>0 && x%%1==0)
-    }
-  return(ans)
+# Check if no entry contains NULL
+# Returns TRUE  if no entries are NULL
+#         FALSE if at least one entry is NULL
+assert_no_null_entries <- function(df){
+  return(!any(is.null(df)))
 }
-# test_positive_integers(simulated_data$population)
+
+# Check if no entry contains NA (also checks for NaN)
+# Returns TRUE  if no entries are NA (and NaN)
+#         FALSE if at least one entry is NA (or NaN)
+assert_no_na_entries <- function(df){
+  return(!any(is.na(df)))
+}
+
+test_data_valid <- function(df, nrows, ncols){
+  return(assert_row_count(df, nrows) && 
+           assert_col_count(df, ncols) &&
+           assert_no_na_entries(df) &&
+           assert_no_null_entries(df))
+}
 
 # Ref: https://www.statology.org/r-loop-through-data-frame-columns/
+# Check if all entries in given columns are positive integers
+# Returns TRUE  if all entries are positive integers
+#         FALSE otherwise
+test_positive_integers <- function(cols){
+  check <- sapply(unlist(cols), FUN=function(x){
+    return(is.numeric(x) && x>0 && x%%1==0)
+  })
+  return(all(check))
+}
+
+# Check if all entries in given columns are positive decimals
+# Returns TRUE  if all entries are positive decimals
+#         FALSE otherwise
 test_positive_decimals <- function(cols){
   check <- sapply(unlist(cols), FUN=function(x){
     return(is.numeric(x) && x>0)
@@ -103,4 +107,17 @@ test_positive_decimals <- function(cols){
   return(all(check))
 }
 
-ans <- test_positive_decimals(select(simulated_data, homicide_rate, assault_rate))
+# Check if all entries in given columns are strings
+# Returns TRUE  if all entries are positive decimals
+#         FALSE otherwise
+test_positive_decimals <- function(cols){
+  check <- sapply(unlist(cols), FUN=function(x){
+    return(is.numeric(x) && x>0)
+  })
+  return(all(check))
+}
+
+### TESTING THE SIMULATED DATA
+test_data_valid(simulated_data, 158, 8)
+test_positive_integers(select(simulated_data, neighbourhood_id, population, thefts, assaults, homicides))
+test_positive_decimals(select(simulated_data, theft_rate, assault_rate, homicide_rate))
